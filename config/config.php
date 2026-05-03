06f61bc46baee7c70ec07ab0940525cb89e8bd9f
Dokka <dokkadoudaev@gmail.com>
2026-05-03 02:36:50 +0200

    fix HTTPS detection behind Railway proxy

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@ header('Content-Type: text/html; charset=utf-8');
 define('BASE_PATH', dirname(__DIR__));
 
 // url de base
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') ? 'https' : 'http';
 $host = $_SERVER['HTTP_HOST'];
 $script = dirname($_SERVER['SCRIPT_NAME']);
 define('BASE_URL', $protocol . '://' . $host . ($script === '/' ? '' : $script));
